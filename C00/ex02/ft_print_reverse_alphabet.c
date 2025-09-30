5e318c224ddfd5bc66eb20c641802f6b83c1903f
yunikme <yuniki.mediayati@gmail.com>
2025-09-30 18:23:13 +0200

    create c file to display reverse alphabet

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ymediaya <ymediaya@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/04 10:51:01 by ymediaya          #+#    #+#             */
+/*   Updated: 2025/09/05 10:57:12 by ymediaya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return 0;
+}*/
