e9dd34e1133fd8f91840c2083b8710f3a5c0404e
yunikme <yuniki.mediayati@gmail.com>
2025-09-30 18:21:49 +0200

    create c file to print alphabet

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ymediaya <ymediaya@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/03 18:25:30 by ymediaya          #+#    #+#             */
+/*   Updated: 2025/09/05 10:54:42 by ymediaya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		write(1, &letter, 1);
+		letter++;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_alphabet();
+	return 0;
+}*/
