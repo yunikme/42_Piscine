9dc99185b7d777b2868a4aafb4414f9e8b5ad234
yunikme <yuniki.mediayati@gmail.com>
2025-10-10 09:15:15 +0200

    C07

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+char    *ft_strdup(char *src)
+{
+    int i;
+
+    i = 0;
+    while (src[i] != '\0')
+    {
+        i++;
+    }
+    char *target = (char *)malloc((i + 1) * sizeof(char));
+    if (target == NULL)
+        return 0;
+    int index;
+    index = 0;
+    while (i >= index)
+    {
+        target[index] = src[index];
+        index++;
+    }
+    return target;
+}
+int main()
+{
+    char* src = "saya bisa makan";
+    char* target = ft_strdup(src);
+    char* target1 = strdup(src);
+    printf("%s\n", target);
+    printf("%s\n", target1);
+
+}
